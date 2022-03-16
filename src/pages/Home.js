b6d201e2fa0ed3228b8959244df7c81ef09ba019
Taha Ebaed <tahaebaed2@gmail.com>
2022-03-16 10:46:21 +0200

    update Error Message in [login] and set redirct from home when [user] not logged

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../store/context/AuthContext'
+import { AuthContext } from '../store/context/AuthContext';
 import { auth } from '../store/context/FireContext';
 
 const Home = () => {
-  const {user}=useContext(AuthContext)
-  const navigate=useNavigate()
-  const logOut=()=>{
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const logOut = () => {
     auth.signOut();
-    if(!user){    navigate("/signIn")  }
-  }
-  console.log(user,"hello from home");
-  return (
-    <div> "hello"
-    <button onClick={logOut}>LogOut</button>
-    </div>
-  )
-}
+    if (!user) {
+      navigate('/signIn');
+    }
+  };
 
-export default Home
+  console.log(user, 'hello from home');
+  return user ? (
+    <div>
+      "hello"
+      <button onClick={logOut}>LogOut</button>
+    </div>
+  ) : (
+    (setTimeout(() => navigate('/signIn')), 1500)
+  );
+};
+
+export default Home;
