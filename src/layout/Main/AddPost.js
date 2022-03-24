48b4e39e0d06b290f2c6b5888d59ebb00c7786d8
Taha Ebaed <tahaebaed2@gmail.com>
2022-03-24 16:46:36 +0200

    fix [vercel] deploying pug

--- src/layout/Main/AddPost.js
+++ src/layout/Main/AddPost.js
@@ -1,22 +1,19 @@
-
 import { Drawer } from '@mui/material';
 import React, { useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import AddPostPopup from './AddPostPopup';
 const AddPost = () => {
   const [popupClose, setpopupClose] = useState(false);
   const [trigger, settrigger] = useState(false);
 
-  const handleOpenPopup = (e) => {
+  const handleOpenPopup = e => {
     settrigger(true);
     setpopupClose(true);
-
   };
   return (
-
     <>
       <Row>
-     <Col m='1' className='main-add_post'>
+        <Col m='1' className='main-add_post'>
           <img
             className='main-add_post-img'
             src='https://picsum.photos/85'
@@ -27,21 +24,21 @@ const AddPost = () => {
           <div className='main-add_post-input' onClick={handleOpenPopup}>
             good morning, lets hit news...
           </div>
-         <Button variant='text' className='main-add_post-button'>
-          <i className='bi bi-images' /> Add Photo
-        </Button>
-        <Button variant='text' className='main-add_post-button ms-0'>
-          <i class='bi bi-camera-video'></i> Add Video
-        </Button>
-        <Button variant='text' className='main-add_post-button ms-0'>
-          <i class='bi bi-calendar-event'></i> Add Event
-        </Button>
+          <Button variant='text' className='main-add_post-button'>
+            <i className='bi bi-images' /> Add Photo
+          </Button>
+          <Button variant='text' className='main-add_post-button ms-0'>
+            <i class='bi bi-camera-video'></i> Add Video
+          </Button>
+          <Button variant='text' className='main-add_post-button ms-0'>
+            <i class='bi bi-calendar-event'></i> Add Event
+          </Button>
         </Col>
       </Row>
-      <div className="drawer">
+      <div className='drawer'>
         <Drawer
-          style={{ backgroundColor: "transparent " }}
-          anchor="top"
+          style={{ backgroundColor: 'transparent ' }}
+          anchor='top'
           open={popupClose}
           onClose={() => setpopupClose(false)}
         >
